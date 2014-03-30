a3a53e00565abb10fb6225a38a80a4fcdb2a3e58
kmartin003 <kpmartin@wpi.edu>
2014-03-30 18:27:31 -0400

    Tests isEnded()
    tests isEnded() to see if it ends when all users have voted
    kpmartin

--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/model/GameTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/model/GameTest.java
@@ -3,10 +3,12 @@ package edu.wpi.cs.wpisuitetng.modules.planningpoker.model;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.junit.Test;
 
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.GameModel;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.GameModel.GameStatus;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.GameModel.GameType;
@@ -16,17 +18,38 @@ import edu.wpi.cs.wpisuitetng.modules.planningpoker.model.GameModel.GameType;
  *
  */
 public class GameTest {
-    
+	
     @Test
     public void TestRequirementEndsAfterDeadline() {
-        GameModel testgame = new GameModel(1, "Test Game", "something", null, new Date(System.currentTimeMillis() - 100000), GameType.DISTRIBUTED, GameStatus.PENDING);
+    	ArrayList<User> users = new ArrayList<User>();
+    	User kevin = new User("Kevin", "kpmartin", "password", 1);
+    	users.add(kevin);
+        GameModel testgame = new GameModel(1, "Test Game", "something", null, new Date(System.currentTimeMillis() - 100000), GameType.DISTRIBUTED, GameStatus.PENDING, users);
         assertTrue(testgame.isEnded());
     }
     
     @Test
     public void TestRequirementNotCompleteBeforeDeadline() {
-        GameModel testgame = new GameModel(2, "Test Game", "something", null, new Date(System.currentTimeMillis() + 100000000), GameType.DISTRIBUTED, GameStatus.PENDING);
+    	ArrayList<User> users = new ArrayList<User>();
+    	User kevin = new User("Kevin", "kpmartin", "password", 1);
+    	users.add(kevin);
+        GameModel testgame = new GameModel(2, "Test Game", "something", null, new Date(System.currentTimeMillis() + 100000000), GameType.DISTRIBUTED, GameStatus.PENDING, users);
         assertFalse(testgame.isEnded());
     }
     
+    @Test
+    public void TestNotAllUsersVoted() {
+    	ArrayList<User> users = new ArrayList<User>();
+    	User kevin = new User("Kevin", "kpmartin", "password", 1);
+    	users.add(kevin);
+    	GameModel testgame = new GameModel(3, "Test Game", "something", null, new Date(System.currentTimeMillis() + 100000000), GameType.DISTRIBUTED, GameStatus.PENDING, users);
+    	assertFalse(testgame.isEnded());
+    }
+    
+    @Test
+    public void TestAllUsersVoted() {
+    	ArrayList<User> users = new ArrayList<User>();
+    	GameModel testgame = new GameModel(3, "Test Game", "something", null, new Date(System.currentTimeMillis() + 100000000), GameType.DISTRIBUTED, GameStatus.PENDING, users);
+    	assertFalse(testgame.isEnded());
+    }
 }
