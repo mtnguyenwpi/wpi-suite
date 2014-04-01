75edbf41439e298a0cb1029b9094f5ffdb686f95
lhunker <lukas.hunker@gmail.com>
2014-03-31 21:11:06 -0400

    Making estimate implement comparable
    adding compareto method to estimates so they can be sorted
    
    Authors: lhunker, dbtrue, nfbrown

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/model/Estimate.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/model/Estimate.java
@@ -5,7 +5,7 @@ import com.google.gson.Gson;
 import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
 import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 
-public class Estimate extends AbstractModel {
+public class Estimate extends AbstractModel implements Comparable<Estimate>{
 	
 	private User user;
 	private float estimate;
@@ -39,5 +39,18 @@ public class Estimate extends AbstractModel {
 	public String toJSON() {
 		return new Gson().toJson(this, Estimate.class);
 	}
+
+    @Override
+    public int compareTo(Estimate arg0) {
+        if(estimate > arg0.getEstimate()){
+            return 1;
+        }
+        else if (estimate == arg0.getEstimate()){
+            return 0;
+        }
+        else{
+            return -1;
+        }
+    }
 
 }
