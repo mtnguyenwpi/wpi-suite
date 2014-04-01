a9555ceb31bdf12f6ea27049b890911e68f33c96
Nick Brown <nfbrown@wpi.edu>
2014-03-31 22:03:59 -0400

    Cleans up EmailController and sets the default message body.
    The default message body is set to the username of the game creator and a quick message saying that a game has begun.
    
    Authors: nfbrown, dcwethern, ellychou

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/EmailController.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/EmailController.java
@@ -2,8 +2,6 @@ package edu.wpi.cs.wpisuitetng.modules.planningpoker.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -14,6 +12,7 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import edu.wpi.cs.wpisuitetng.janeway.config.ConfigManager;
 import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 import edu.wpi.cs.wpisuitetng.network.Network;
 import edu.wpi.cs.wpisuitetng.network.Request;
@@ -21,9 +20,11 @@ import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
 
 /**
  * This class controls the sending of email notifications
- * The code for sending emails was based on code at http://www.mkyong.com/java/javamail-api-sending-email-via-gmail-smtp-example/
+ * The code for sending emails was based on code at
+ * http://www.mkyong.com/java/javamail-api-sending-email-via-gmail-smtp-example/
+ * 
  * @author nfbrown, szhou, dcwethern
- *
+ * 
  */
 public class EmailController implements ActionListener {
     
@@ -37,78 +38,84 @@ public class EmailController implements ActionListener {
     private String to;
     
     public EmailController() {
-        this.body = "This is a test";
-        this.subject = "TEST!!!";
+        body = ConfigManager.getConfig().getUserName()
+                + " has created a new Planning Poker game. Please open Janeway and make your estimates!";
+        subject = "A planning poker game is about to begin";
         observer = new UserRequestObserver(this);
         requestUsers();
         parseEmails();
     }
-
+    
     public void send() {
- 
+        
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
- 
+        
         Session session = Session.getInstance(props,
-          new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-          });
- 
+                new javax.mail.Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+        
         try {
- 
+            
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("team9wpi@gmail.com"));
             message.setRecipients(Message.RecipientType.TO,
-                InternetAddress.parse(to));
+                    InternetAddress.parse(to));
             message.setSubject(subject);
             message.setText(body);
- 
+            
             Transport.send(message);
- 
+            
             System.out.println("Done");
- 
-        } catch (MessagingException e) {
+            
+        }
+        catch (MessagingException e) {
             throw new RuntimeException(e);
         }
     }
     
     private void requestUsers() {
-        final Request request = Network.getInstance().makeRequest("core/user", HttpMethod.GET);
+        final Request request = Network.getInstance().makeRequest("core/user",
+                HttpMethod.GET);
         request.addObserver(observer); // add an observer to process the response
         request.send(); // send the request
     }
     
     public void receivedUsers(User[] users) {
-            if (users != null) {
-                this.users = users;
-            } else {
-                // TODO: error handling
-            }
+        if (users != null) {
+            this.users = users;
+        }
+        else {
+            // TODO: error handling
+        }
     }
     
     private void parseEmails() {
         String s = "";
         int i;
         for (i = 0; i < users.length - 1; i++) {
-            if (users[i].getEmail() != null) {
+            if (users[i].getEmail() != null
+                    && !users[i].getUsername().equals(
+                            ConfigManager.getConfig().getUserName())) {
                 s += users[i].getEmail() + ",";
             }
         }
         if (users[i].getEmail() != null) {
             s += users[i].getEmail();
         }
-        this.to = s;
+        to = s;
     }
-
+    
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        
+        send();
     }
     
 }
