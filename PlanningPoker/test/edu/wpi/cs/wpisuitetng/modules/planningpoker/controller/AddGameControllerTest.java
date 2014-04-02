3643b9b51bf9e8d736a6695e0b0f8e3ed11f6097
andrewwhan <awhan@wpi.edu>
2014-04-01 22:58:43 -0400

    Code formatting

--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/AddGameControllerTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/AddGameControllerTest.java
@@ -1,27 +1,28 @@
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.controller;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * 
  * @author Andrew
- *
+ * 
  */
 public class AddGameControllerTest {
     
     static AddGameController instance;
     
     @BeforeClass
-    static public void setUpBeforeClass(){
-        instance = AddGameController.getInstance();
+    static public void setUpBeforeClass() {
+        AddGameControllerTest.instance = AddGameController.getInstance();
     }
     
     @Test
     public void testGetInstance() {
-        assertEquals("A new instance is not the same as the previous instance", instance, AddGameController.getInstance());
+        Assert.assertEquals(
+                "A new instance is not the same as the previous instance",
+                AddGameControllerTest.instance, AddGameController.getInstance());
     }
     
 }
--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/GetGamesControllerTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/GetGamesControllerTest.java
@@ -1,9 +1,8 @@
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.controller;
 
-import static org.junit.Assert.*;
-
 import java.util.Date;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -24,29 +23,55 @@ public class GetGamesControllerTest {
     static GameModel[] gamesToAdd;
     
     @BeforeClass
-    static public void setUpBeforeClass(){
-        instance = GetGamesController.getInstance();
-        nullGame = new GameModel();
-        game1 = new GameModel(1, "Test Game 1", "Live Game that just ended", null, new Date(), GameType.LIVE, GameStatus.COMPLETE);
-        game2 = new GameModel(2, "Test Game 2", "Distributed Game that will end in 5 seconds", null, new Date(System.currentTimeMillis()+5000), GameType.DISTRIBUTED, GameStatus.PENDING);
-        game3 = new GameModel(3, "Test Game 3", "Live Game with end time in 10 seconds, but already manually ended", null, new Date(System.currentTimeMillis()+10000), GameType.LIVE, GameStatus.COMPLETE);
-        game4 = new GameModel(4, "Test Game 4", "Distributed Game that has end time 10 seconds ago but hasn't been updated to be complete yet", null, new Date(System.currentTimeMillis()-10000), GameType.DISTRIBUTED, GameStatus.PENDING);
-        list = GameListModel.getInstance();
-        gamesToAdd = new GameModel[] {game1, game2, game3, game4};
+    static public void setUpBeforeClass() {
+        GetGamesControllerTest.instance = GetGamesController.getInstance();
+        GetGamesControllerTest.nullGame = new GameModel();
+        GetGamesControllerTest.game1 = new GameModel(1, "Test Game 1",
+                "Live Game that just ended", null, new Date(), GameType.LIVE,
+                GameStatus.COMPLETE);
+        GetGamesControllerTest.game2 = new GameModel(2, "Test Game 2",
+                "Distributed Game that will end in 5 seconds", null, new Date(
+                        System.currentTimeMillis() + 5000),
+                GameType.DISTRIBUTED, GameStatus.PENDING);
+        GetGamesControllerTest.game3 = new GameModel(
+                3,
+                "Test Game 3",
+                "Live Game with end time in 10 seconds, but already manually ended",
+                null, new Date(System.currentTimeMillis() + 10000),
+                GameType.LIVE, GameStatus.COMPLETE);
+        GetGamesControllerTest.game4 = new GameModel(
+                4,
+                "Test Game 4",
+                "Distributed Game that has end time 10 seconds ago but hasn't been updated to be complete yet",
+                null, new Date(System.currentTimeMillis() - 10000),
+                GameType.DISTRIBUTED, GameStatus.PENDING);
+        GetGamesControllerTest.list = GameListModel.getInstance();
+        GetGamesControllerTest.gamesToAdd = new GameModel[] {
+                GetGamesControllerTest.game1, GetGamesControllerTest.game2,
+                GetGamesControllerTest.game3, GetGamesControllerTest.game4 };
     }
     
     @Test
     public void testGetInstance() {
-        assertEquals("A new instance is not the same as the previous instance", instance, GetGamesController.getInstance());
+        Assert.assertEquals(
+                "A new instance is not the same as the previous instance",
+                GetGamesControllerTest.instance,
+                GetGamesController.getInstance());
     }
     
     @Test
     public void testReceivedGames() {
-        instance.receivedGames(new GameModel[] {game1, game2, game3, game4});
-        assertTrue(GameListModel.getInstance().getGames().contains(game1));
-        assertTrue(GameListModel.getInstance().getGames().contains(game2));
-        assertTrue(GameListModel.getInstance().getGames().contains(game3));
-        assertTrue(GameListModel.getInstance().getGames().contains(game4));
+        GetGamesControllerTest.instance.receivedGames(new GameModel[] {
+                GetGamesControllerTest.game1, GetGamesControllerTest.game2,
+                GetGamesControllerTest.game3, GetGamesControllerTest.game4 });
+        Assert.assertTrue(GameListModel.getInstance().getGames()
+                .contains(GetGamesControllerTest.game1));
+        Assert.assertTrue(GameListModel.getInstance().getGames()
+                .contains(GetGamesControllerTest.game2));
+        Assert.assertTrue(GameListModel.getInstance().getGames()
+                .contains(GetGamesControllerTest.game3));
+        Assert.assertTrue(GameListModel.getInstance().getGames()
+                .contains(GetGamesControllerTest.game4));
     }
     
 }
