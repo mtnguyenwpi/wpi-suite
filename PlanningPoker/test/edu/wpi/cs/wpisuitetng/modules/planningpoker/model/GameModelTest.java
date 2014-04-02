3b91b25d318ec1c3654aca043a005f876978a9e6
kmartin003 <kpmartin@wpi.edu>
2014-04-01 21:32:56 -0400

    Updates the constructors for GameModel
    Added the userList variable to the constructor
    kpmartin

--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/model/GameModelTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/model/GameModelTest.java
@@ -29,10 +29,10 @@ public class GameModelTest {
     @Before
     public void setUp(){
         nullGame = new GameModel();
-        game1 = new GameModel(1, "Test Game 1", "Live Game that just ended", null, new Date(), GameType.LIVE, GameStatus.COMPLETE);
-        game2 = new GameModel(2, "Test Game 2", "Distributed Game that will end in 5 seconds", null, new Date(System.currentTimeMillis()+5000), GameType.DISTRIBUTED, GameStatus.PENDING);
-        game3 = new GameModel(3, "Test Game 3", "Live Game with end time in 10 seconds, but already manually ended", null, new Date(System.currentTimeMillis()+10000), GameType.LIVE, GameStatus.COMPLETE);
-        game4 = new GameModel(4, "Test Game 4", "Distributed Game that has end time 10 seconds ago but hasn't been updated to be complete yet", null, new Date(System.currentTimeMillis()-10000), GameType.DISTRIBUTED, GameStatus.PENDING);
+        game1 = new GameModel(1, "Test Game 1", "Live Game that just ended", null, new Date(), GameType.LIVE, GameStatus.COMPLETE, null);
+        game2 = new GameModel(2, "Test Game 2", "Distributed Game that will end in 5 seconds", null, new Date(System.currentTimeMillis()+5000), GameType.DISTRIBUTED, GameStatus.PENDING, null);
+        game3 = new GameModel(3, "Test Game 3", "Live Game with end time in 10 seconds, but already manually ended", null, new Date(System.currentTimeMillis()+10000), GameType.LIVE, GameStatus.COMPLETE, null);
+        game4 = new GameModel(4, "Test Game 4", "Distributed Game that has end time 10 seconds ago but hasn't been updated to be complete yet", null, new Date(System.currentTimeMillis()-10000), GameType.DISTRIBUTED, GameStatus.PENDING, null);
     }
     
     @Test
