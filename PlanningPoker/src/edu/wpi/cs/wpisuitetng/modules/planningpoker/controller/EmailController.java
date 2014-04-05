15a198cc2e0331eb69e219e5654fb6d9efc4f018
Nick Brown <nfbrown@wpi.edu>
2014-04-04 20:50:11 -0400

    Probably fixes bugs with sending email on most computers
    There was an issue where emails failed to send on some computers. These changes allowed emails to send on those computers.
    
    Authors: nfbrown, dcwethern

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/EmailController.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/EmailController.java
@@ -1,10 +1,6 @@
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.controller;
 
-import java.util.ArrayList;
-import java.util.Properties;
-
 import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
 
 import org.apache.commons.mail.*;
 
@@ -16,8 +12,6 @@ import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
 
 /**
  * This class controls the sending of email notifications
- * The code for sending emails was based on code at
- * http://www.mkyong.com/java/javamail-api-sending-email-via-gmail-smtp-example/
  * 
  * @author nfbrown, szhou, dcwethern
  * 
@@ -29,7 +23,7 @@ public class EmailController {
     private String username = "team9wpi";
     private String from = "team9wpi@gmail.com";     // GMail user name
     private String password = "team9ftw";           // GMail password
-    private String subject = "A planning poker game is about to begin";
+    private String subject = "A new Planning Poker game has begun!";
     private String body;
     
     /**
@@ -56,69 +50,29 @@ public class EmailController {
     
     /**
      * Sends an email to all users that are not the creator of the game
-     * @throws EmailException 
-     * @throws AddressException 
+     * 
+     * @throws EmailException
+     * @throws AddressException
      */
     private void sendEmails() {
-        /*
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-        
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-                    @Override
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(from, password);
-                    }
-                });
-        
         setBody();
-        
-        Message message = new MimeMessage(session);
+        System.setProperty("java.net.preferIPv4Stack", "true");
         try {
-            message.setFrom(new InternetAddress(this.from));
-            message.setSubject(this.subject);
-            
             for (User u : users) {
                 if (u.getEmail() != null) {
-                    if (!u.getUsername().equals(ConfigManager.getConfig().getUserName())) {
-                        message.setRecipient(Message.RecipientType.TO, new InternetAddress(u.getEmail()));
-                        message.setText("Dear " + u.getName() + ",\n" + body);
-                        Transport.send(message);
-                    }
-                }
-            }
-        }
-        catch (MessagingException e) {
-            System.err.println("Email notifications failed to send");
-            e.printStackTrace();
-        }
-        */
-        
-        Email email = new SimpleEmail();
-        email.setHostName("smtp.gmail.com");
-        email.setSmtpPort(587);
-        email.setAuthenticator(new DefaultAuthenticator(username, password));
-        email.setSSLOnConnect(true);
-        try {
-            email.setSubject("A new Planning Poker game has begun!");
-            for (User u : users) {
-                if (u.getEmail() != null) {
-                    ArrayList<InternetAddress> to = new ArrayList<InternetAddress>();
-                    to.add(new InternetAddress(u.getEmail()));
-                    email.setTo(to);
+                    Email email = new SimpleEmail();
+                    email.setHostName("smtp.gmail.com");
+                    email.setSmtpPort(587);
+                    email.setAuthenticator(new DefaultAuthenticator(username, password));
+                    email.setSSLOnConnect(true);
+                    email.addTo(u.getEmail());
+                    email.setSubject(subject);
+                    email.setFrom(from);
                     email.setMsg("Dear " + u.getName() + ",\n" + body);
                     email.send();
                 }
             }
         } catch (EmailException e) {
-            System.err.println("Email failed to send");
-            e.printStackTrace();
-        }
-        catch (AddressException e) {
             System.err.println("Email failed to send");
             e.printStackTrace();
         }
